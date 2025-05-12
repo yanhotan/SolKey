5a752881991bad423ee736542d3aab06ce79149d
yanhotan <130077767+yanhotan@users.noreply.github.com>
2025-05-12 17:05:38 +0800

    remove mongodb

--- solkey-backend/init-mongo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sleep 10  # Wait for MongoDB to start
-mongosh admin -u "${MONGO_INITDB_ROOT_USERNAME}" -p "${MONGO_INITDB_ROOT_PASSWORD}" --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "localhost:27017"}]})'
